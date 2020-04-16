c7a66c8364b1b6a8a2df1199eff68ca7d4adf10a
JossWritesCode <joscelynrose@gmail.com>
2020-04-15 20:07:11 -0400

    fixed recipe batches

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# import argparse
+import argparse
 
 
 def find_max_profit(prices):
@@ -20,12 +20,13 @@ def find_max_profit(prices):
     return max_profit
 
 
-print(find_max_profit([1050, 270, 1540, 3800, 2]), "should be 3530")
+if __name__ == '__main__':
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-# if __name__ == '__main__':
-#   # This is just some code to accept inputs from the command line
-#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
-#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-#   args = parser.parse_args()
-
-#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
